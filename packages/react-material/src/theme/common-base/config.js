5fa9793da834c6f04c1b3e9e116b0e335f086e51
kevan <kevan@jahia.com>
2018-09-26 16:45:43 +0200

    BACKLOG-8607: Adapt z-index of the themes to deal with GWT engines

--- packages/react-material/src/theme/common-base/config.js
+++ packages/react-material/src/theme/common-base/config.js
@@ -5,6 +5,14 @@ const commonBaseThemeConfig = {
     palette: commonPalette,
     overrides: {
         MuiTablePagination: commonTablePagination
+    },
+    zIndex: {
+        mobileStepper: 1000,
+        appBar: 1004,
+        drawer: 1008,
+        modal: 1012,
+        snackbar: 1400,
+        tooltip: 1500
     }
 };
 
