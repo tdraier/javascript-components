d47242420ff0d04b4d50e76c9f7cc1d7d541f841
tdraier <tdraier@jahia.com>
2018-10-11 13:14:29 +0200

    Merge branch 'master' of /Users/tdraier/src/javascript-components with conflicts.

--- packages/react-material/src/theme/common-base/config.js
+++ packages/react-material/src/theme/common-base/config.js
@@ -1,18 +1,16 @@
 import {commonPalette} from "./palette";
 import {commonTablePagination} from "./overrides/table";
-import {typography} from './typography';
 
 const commonBaseThemeConfig = {
     palette: commonPalette,
-    typography: typography,
     overrides: {
         MuiTablePagination: commonTablePagination
     },
     zIndex: {
         mobileStepper: 1000,
-        appBar: 1004,
-        drawer: 1008,
-        modal: 1012,
+        appBar: 1100,
+        drawer: 1200,
+        modal: 1300,
         snackbar: 1400,
         tooltip: 1500
     }
