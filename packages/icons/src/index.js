f07ac108684035194d503970b2a32c2590aa902e
oyevstafyev <oievstafiev@jahia.com>
2018-10-23 18:06:48 +0200

    [BACKLOG-8794] Missing index.js update to re-export new function

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -1,3 +1,4 @@
 export {default as NavMenuTextIcon} from './NavMenuTextIcon'
 export {default as PageIcon} from './PageIcon'
 export {default as VirtualsiteIcon} from './VirtualsiteIcon'
+export {getIcon} from './iconRegistry'
