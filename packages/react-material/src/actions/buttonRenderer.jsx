66cd742ad1269ed3a0a2597ebc84754bfd8f5dfb
tdraier <tdraier@jahia.com>
2018-11-11 14:31:30 +0100

    BACKLOG-8803 : added action key, icon in button

--- packages/react-material/src/actions/buttonRenderer.jsx
+++ packages/react-material/src/actions/buttonRenderer.jsx
@@ -2,8 +2,9 @@ import {Button} from "@material-ui/core";
 import React from "react";
 import {I18n} from 'react-i18next';
 
-let buttonRenderer = (buttonProps) => ({context}) => <I18n ns={context.buttonLabelNamespace}>
+let buttonRenderer = (buttonProps, showIcon) => ({context}) => <I18n ns={context.buttonLabelNamespace}>
     {t => <Button data-sel-role={context.key} onClick={(e) => { e.stopPropagation(); context.onClick(context, e)}} {...buttonProps}>
+        {showIcon && context.buttonIcon}
         <span dangerouslySetInnerHTML={{__html:t(context.buttonLabel, context.buttonLabelParams)}}/>
     </Button>}
 </I18n>;
