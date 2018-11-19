444a49bd941a00e49e6b26d4c23ce5120ba7b262
Sergiy Shyrkov <sshyrkov@jahia.com>
2018-11-19 16:11:14 +0100

    Revert "[BACKLOG-8981] Correctly sort actions by priority"
    This reverts commit c3689b86fe0d0c8d172b659c31f1701d262e242d.

--- packages/react-material/src/actions/DisplayActions.jsx
+++ packages/react-material/src/actions/DisplayActions.jsx
@@ -11,11 +11,7 @@ class DisplayActions extends React.Component {
     render() {
         const {target, context, render} = this.props;
 
-        const actionsToDisplay = _.filter(actionsRegistry.getAll(), (action) => _.includes(_.map(action.target, "id"), target));
-        actionsToDisplay = _.sortBy(actionsToDisplay, [function(o) {
-            let target = _.find(o.target, function(t) { return t.id === target; });
-            return target && target.priority && target.priority != 0 ? target.priority : 'undefined'; 
-        }]);
+        const actionsToDisplay = _.sortBy(_.filter(actionsRegistry.getAll(), (action) => _.includes(_.map(action.target, "id"), target)), "target.priority");
 
         return _.map(actionsToDisplay, action => <DisplayAction key={action.key} context={context} actionKey={action.key} render={render}/>)
     }
