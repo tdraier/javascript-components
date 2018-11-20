ea182cbe7581d19200dac013aa33d5427e357e8c
wdelenclos <wdelenclos@gmail.com>
2018-11-20 13:48:02 +0100

    [Add] line height typography

--- packages/ds-mui-theme/src/theme/typography.js
+++ packages/ds-mui-theme/src/theme/typography.js
@@ -10,81 +10,92 @@ const typography = {
         "fontFamily": "Nunito Sans",
         "fontSize": "96px",
         "fontWeight": 300,
-        "letterSpacing": "-1.5px"
+        "letterSpacing": "-1.5px",
+        "lineHeight" : "1"
     },
     "h2": {
         "fontFamily": "Nunito Sans",
         "fontSize": "60px",
         "fontWeight": 300,
-        "letterSpacing": "-0.5px"
+        "letterSpacing": "-0.5px",
+        "lineHeight" : "1"
 
     },
     "h3": {
         "fontFamily": "Nunito Sans",
         "fontSize": "48px",
         "fontWeight": 400,
+        "lineHeight" : "1.04"
     },
     "h4": {
         "fontFamily": "Nunito Sans",
         "fontSize": "34px",
         "fontWeight": 400,
-        "letterSpacing": "0.25px"
+        "letterSpacing": "0.25px",
+        "lineHeight" : "1.17"
 
     },
     "h5": {
         "fontFamily": "Nunito Sans",
         "fontSize": "24px",
-        "fontWeight": 400
+        "fontWeight": 400,
+        "lineHeight" : "1.33"
     },
     "h6": {
         "fontFamily": "Nunito Sans",
         "fontSize": "20px",
         "fontWeight": 600,
-        "letterSpacing": "0.15px"
+        "letterSpacing": "0.15px",
+        "lineHeight" : "1.60"
     },
     "subtitle1": {
         "fontFamily": "Nunito Sans",
         "fontSize": "16px",
         "fontWeight": 400,
-        "letterSpacing": "0.15px"
+        "letterSpacing": "0.15px",
+        "lineHeight" : "1.75"
     },
     "subtitle2": {
         "fontFamily": "Nunito Sans",
         "fontSize": "16px",
         "fontWeight": 600,
-        "letterSpacing": "0.10px"
+        "letterSpacing": "0.10px",
+        "lineHeight" : "1.57"
     },
     "body1": {
         "fontFamily": "Nunito Sans",
         "fontSize": "16px",
         "fontWeight": 400,
-        "letterSpacing": "0.44px"
+        "letterSpacing": "0.44px",
+        "lineHeight" : "1.50"
     },
     "body2": {
         "fontFamily": "Nunito Sans",
         "fontSize": "14px",
         "fontWeight": 400,
-        "letterSpacing": "0.25px"
-
+        "letterSpacing": "0.25px",
+        "lineHeight" : "1.50"
     },
     "button": {
         "fontFamily": "Nunito Sans",
         "fontSize": "14px",
         "fontWeight": 600,
-        "letterSpacing": "1.35px"
-
+        "letterSpacing": "1.35px",
+        "lineHeight" : "1.50"
     },
     "caption": {
         "fontFamily": "Nunito Sans",
         "fontSize": "12px",
         "fontWeight": 400,
-        "letterSpacing": "0.2px"
+        "letterSpacing": "0.2px",
+        "lineHeight" : "1.66"
     },
     "overline": {
         "fontFamily": "Nunito Sans",
         "fontSize": "10px",
         "fontWeight": 400,
-        "letterSpacing": "0.5px"
+        "letterSpacing": "0.5px",
+        "lineHeight" : "2.66"
     }
 };
 
