36e32d06bbf3ecb1be8364b312f15b2c6eb24e43
tdraier <tdraier@jahia.com>
2018-12-20 15:33:17 +0100

    BACKLOG-9237 : added fallback for ie

--- packages/ds-mui-theme/src/theme/typography.js
+++ packages/ds-mui-theme/src/theme/typography.js
@@ -1,35 +1,35 @@
 const typography = {
     root: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         light: 300,
         regular: 400,
         semiBold: 600,
         fontSize : "16px",
         fontWeight : 400
     },
-    fontFamily: "Nunito Sans",
+    fontFamily: "Nunito Sans, sans-serif",
     h1: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "96px",
         fontWeight: 300,
         letterSpacing: "-1.5px",
         lineHeight : "1"
     },
     h2: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "60px",
         fontWeight: 300,
         letterSpacing: "-0.5px",
         lineHeight : "1"
     },
     h3: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "48px",
         fontWeight: 400,
         lineHeight : "1.04"
     },
     h4: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "34px",
         fontWeight: 400,
         letterSpacing: "0.25px",
@@ -37,48 +37,48 @@ const typography = {
 
     },
     h5: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "24px",
         fontWeight: 400,
         lineHeight : "1.33"
     },
     h6: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "20px",
         fontWeight: 600,
         letterSpacing: "0.15px",
         lineHeight : "1.60"
     },
     subtitle1: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "16px",
         fontWeight: 400,
         letterSpacing: "0.15px",
         lineHeight : "1.75"
     },
     subtitle2: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "14px",
         fontWeight: 600,
         letterSpacing: "0.10px",
         lineHeight : "1.57"
     },
     body1: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "14px",
         fontWeight: 400,
         letterSpacing: "0.44px",
         lineHeight : "1.50"
     },
     body2: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "13px",
         fontWeight: 400,
         letterSpacing: "0.25px",
         lineHeight : "1.50"
     },
     button: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "14px",
         fontWeight: 600,
         letterSpacing: "1.35px",
@@ -86,14 +86,14 @@ const typography = {
         textTransform: "none"
     },
     caption: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "12px",
         fontWeight: 400,
         letterSpacing: "0.2px",
         lineHeight : "1.66"
     },
     overline: {
-        fontFamily: "Nunito Sans",
+        fontFamily: "Nunito Sans, sans-serif",
         fontSize: "10px",
         fontWeight: 600,
         letterSpacing: "0.5px",
