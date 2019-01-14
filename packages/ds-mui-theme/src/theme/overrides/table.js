3011b2927813045857be6199162712230eab4360
dgaillard <dgaillard@jahia.com>
2019-01-14 15:04:38 +0100

    BACKLOG-9363 Fix padding for table cell with inline actions

--- packages/ds-mui-theme/src/theme/overrides/table.js
+++ packages/ds-mui-theme/src/theme/overrides/table.js
@@ -3,7 +3,7 @@ const dsTable = (theme) => ({
 });
 const dsTableCell = (theme) => ({
     root: {
-        padding: "2px 56px 4px 24px",
+        padding: "0px 56px 4px 24px",
         borderBottom: "none",
         "& svg": {
             verticalAlign:"middle"
