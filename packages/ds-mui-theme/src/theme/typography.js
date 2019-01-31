22868bdb721c83e4ba6849b15c5488189b18dd34
wdelenclos <wdelenclos@gmail.com>
2019-01-31 10:23:50 +0100

    [DESIGN-118] Add formalized typography in theme

--- packages/ds-mui-theme/src/theme/typography.js
+++ packages/ds-mui-theme/src/theme/typography.js
@@ -1,11 +1,11 @@
-const typography = {
+var typography = {
     root: {
         fontFamily: "Nunito Sans, sans-serif",
         light: 300,
         regular: 400,
         semiBold: 600,
-        fontSize : "16px",
-        fontWeight : 400
+        fontSize: "16px",
+        fontWeight: 400
     },
     fontFamily: "Nunito Sans, sans-serif",
     h1: {
@@ -13,76 +13,76 @@ const typography = {
         fontSize: "96px",
         fontWeight: 300,
         letterSpacing: "-1.5px",
-        lineHeight : "1"
+        lineHeight: "1"
     },
     h2: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "60px",
         fontWeight: 300,
         letterSpacing: "-0.5px",
-        lineHeight : "1"
+        lineHeight: "1"
     },
     h3: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "48px",
         fontWeight: 400,
-        lineHeight : "1.04"
+        lineHeight: "1.04"
     },
     h4: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "34px",
         fontWeight: 400,
         letterSpacing: "0.25px",
-        lineHeight : "1.17"
+        lineHeight: "1.17"
 
     },
     h5: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "24px",
         fontWeight: 400,
-        lineHeight : "1.33"
+        lineHeight: "1.33"
     },
     h6: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "20px",
         fontWeight: 600,
         letterSpacing: "0.15px",
-        lineHeight : "1.60"
+        lineHeight: "1.60"
     },
     subtitle1: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "16px",
         fontWeight: 400,
         letterSpacing: "0.15px",
-        lineHeight : "1.75"
+        lineHeight: "1.75"
     },
     subtitle2: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "14px",
         fontWeight: 600,
         letterSpacing: "0.10px",
-        lineHeight : "1.57"
+        lineHeight: "1.57"
     },
     body1: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "14px",
         fontWeight: 400,
         letterSpacing: "0.44px",
-        lineHeight : "1.50"
+        lineHeight: "1.50"
     },
     body2: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "13px",
         fontWeight: 400,
         letterSpacing: "0.25px",
-        lineHeight : "1.50"
+        lineHeight: "1.50"
     },
     button: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "14px",
         fontWeight: 600,
         letterSpacing: "1.35px",
-        lineHeight : "1.50",
+        lineHeight: "1.50",
         textTransform: "none"
     },
     caption: {
@@ -90,16 +90,117 @@ const typography = {
         fontSize: "12px",
         fontWeight: 400,
         letterSpacing: "0.2px",
-        lineHeight : "1.66"
+        lineHeight: "1.66"
     },
     overline: {
         fontFamily: "Nunito Sans, sans-serif",
         fontSize: "11px",
         fontWeight: 600,
         letterSpacing: "0.5px",
-        lineHeight : "2.66",
+        lineHeight: "2.66",
+        textTransform: "none"
+    },
+
+    // Display sizes
+    giga: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 76,
+        fontWeight: 300,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    mega: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 54,
+        fontWeight: 300,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    // Normal sizes
+    alpha: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 36,
+        fontWeight: 300,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    beta: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 28,
+        fontWeight: 300,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    gamma: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 20,
+        fontWeight: 300,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    delta: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 18,
+        fontWeight: 600,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    epsilon: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 16,
+        fontWeight: 600,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    zeta: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 14,
+        fontWeight: 600,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    iota: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 14,
+        fontWeight: 400,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    omega: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 12,
+        fontWeight: 600,
+        letterSpacing: 0,
+        lineHeight: "1.25",
+        textTransform: "none"
+    },
+    // Extra sizes
+    legal: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 11,
+        fontWeight: 400,
+        letterSpacing: 0,
+        lineHeight: "1.5",
+        textTransform: "none"
+    },
+    p: {
+        fontFamily: "Nunito Sans, sans-serif",
+        fontSize: 16,
+        fontWeight: 400,
+        letterSpacing: 0,
+        lineHeight: "1.5",
         textTransform: "none"
     }
+
 };
 
-export {typography}
+export { typography };
