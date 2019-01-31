1a72fc3d49e964c1640af213a62e7f8d838fee5a
tdraier <tdraier@jahia.com>
2019-01-31 17:44:45 +0100

    BACKLOG-9446 : cleaned constants

--- packages/layouts/src/styleConstants.js
+++ packages/layouts/src/styleConstants.js
@@ -1,12 +1,8 @@
 const styleConstants = {
     topBarHeight: 130,
-    treeDrawerWidth: 260,
-    previewDrawerWidth: 600,
     screenMargin: 38,
     leftNavigationWidth: 70,
     leftNavigationDrawerWidth: 289,
-    toolbarHeight: 48,
-    paginationHeight: 56
 };
 
 export default styleConstants;
