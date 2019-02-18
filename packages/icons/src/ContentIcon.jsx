b881a50a510ef4ce03e8de8fa2cb27c5ce9e3c94
Yousria <yousria.benchadi@gmail.com>
2019-02-18 09:59:20 +0100

    BACKLOG-9648 : add Icon to the theme

---
+++ packages/icons/src/ContentIcon.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {pure} from 'recompose';
+import {SvgIcon} from '@material-ui/core';
+
+let ContentIcon = props => (
+    <SvgIcon {...props} style="width:24px;height:24px" viewBox="0 0 24 24">
+        <path fill="#000000" d="M16,15H9V13H16M19,11H9V9H19M19,7H9V5H19M21,1H7C5.89,1 5,1.89 5,3V17C5,18.11 5.9,19 7,19H21C22.11,19 23,18.11 23,17V3C23,1.89 22.1,1 21,1M3,5V21H19V23H3A2,2 0 0,1 1,21V5H3Z" />
+    </SvgIcon>
+);
+
+ContentIcon.displayName = "ContentIcon";
+ContentIcon = pure(ContentIcon);
+ContentIcon.muiName = 'SvgIcon';
+
+ContentIcon.primaryNodeType= 'jnt:content';
+
+export default ContentIcon;
--- packages/icons/src/iconRegistry.js
+++ packages/icons/src/iconRegistry.js
@@ -1,6 +1,7 @@
 import NavMenuTextIcon from "./NavMenuTextIcon";
 import PageIcon from "./PageIcon";
 import VirtualsiteIcon from "./VirtualsiteIcon";
+import ContentIcon from "./ContentIcon";
 import {Folder} from '@material-ui/icons';
 
 const ICON_BY_NODE_TYPE = {
@@ -8,7 +9,8 @@ const ICON_BY_NODE_TYPE = {
     "jnt:page": PageIcon,
     "jnt:virtualsite": VirtualsiteIcon,
     "jnt:folder": Folder,
-    "jnt:contentFolder": Folder
+    "jnt:contentFolder": Folder,
+    "jnt:content" : ContentIcon
 }
 
 function getIcon(nodeType) {
