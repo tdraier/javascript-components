2a12aa1f12b628502e47f3bda0745ea5394bf046
dgaillard <dgaillard@jahia.com>
2019-02-22 14:03:48 +0100

    BACKLOG-9670 implement FormControlLabel in ds-mui-theme

---
+++ packages/ds-mui-theme/src/components/FormControlLabel.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {FormControlLabel as MuiFormControlLabel, withStyles} from '@material-ui/core';
+import * as _ from 'lodash';
+import classnames from 'classnames';
+
+let styles = () => ({
+    /* Styles applied to the root element. */
+    root: {},
+
+    /* Styles applied to the root element if `labelPlacement="start"`. */
+    labelPlacementStart: {},
+
+    /* Styles applied to the root element if `labelPlacement="top"`. */
+    labelPlacementTop: {},
+
+    /* Styles applied to the root element if `labelPlacement="bottom"`. */
+    labelPlacementBottom: {},
+
+    /* Styles applied to the root element if `disabled={true}`. */
+    disabled: {
+        '& svg': {
+            opacity: 1
+        }
+    },
+
+    /* Styles applied to the label's Typography component. */
+    label: {}
+});
+
+const getClasses = ({variant, color, classes: {root, labelPlacementStart, labelPlacementTop, labelPlacementBottom, disabled, label, ...myClasses}}) => ({
+    root: classnames(
+        root,
+        myClasses[variant],
+        myClasses['color' + _.capitalize(color)]
+    ),
+    labelPlacementStart,
+    labelPlacementTop,
+    labelPlacementBottom,
+    disabled,
+    label
+});
+
+const FormControlLabel = withStyles(styles, {name: 'DsFormControlLabel'})(
+    ({variant, color, classes, ...props}) => (
+        <MuiFormControlLabel classes={getClasses({variant, color, classes})} {...props}/>
+    )
+);
+
+FormControlLabel.propTypes = process.env.NODE_ENV !== "production" ? {
+} : {};
+
+FormControlLabel.defaultProps = {
+    labelPlacement: 'end'
+};
+
+FormControlLabel.displayName = 'DsFormControlLabel';
+
+export default FormControlLabel;
