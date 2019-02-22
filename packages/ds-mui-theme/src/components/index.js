dd656ccdb95c846511957c73e1f667625c7d9a21
dgaillard <dgaillard@jahia.com>
2019-02-22 14:18:27 +0100

    BACKLOG-9670 add missing export

--- packages/ds-mui-theme/src/components/index.js
+++ packages/ds-mui-theme/src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Typography} from './Typography';
 export {default as Button} from './Button';
 export {default as Badge} from './Badge';
+export {default as FormControlLabel} from './FormControlLabel';
