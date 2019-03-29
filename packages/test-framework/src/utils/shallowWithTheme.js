544a1f37937312e7b2b326456f3ebc443a460035
akarmanov <alexandrk_@hotmail.com>
2019-03-29 13:38:50 -0400

    BACKLOG-9978 Export shallowWithTheme as default so that it is defined when exported from index.js

--- packages/test-framework/src/utils/shallowWithTheme.js
+++ packages/test-framework/src/utils/shallowWithTheme.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {shallow} from 'enzyme/build';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
-export const shallowWithTheme = (children, options, theme) => {
+const shallowWithTheme = (children, options, theme) => {
     const wrapper = shallow(<MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>, options);
     const instance = wrapper.instance();
     return wrapper.shallow({context: instance.getChildContext()});
 };
+
+export default shallowWithTheme;
