9c3048888e0dcecff9f54b0e82809916570bab42
Wladimir Delenclos <wdelenclos@gmail.com>
2019-03-29 15:29:26 +0100

    BACKLOG-9976 : Adapt tests for ds-mui-theme

--- packages/ds-mui-theme/src/components/Badge.test.js
+++ packages/ds-mui-theme/src/components/Badge.test.js
@@ -6,7 +6,6 @@ import {Badge} from './Badge';
 describe('Badge', () => {
     it('should render correctly in "debug" mode', () => {
         const component = shallow(<Badge debug />);
-
         expect(component).toMatchSnapshot();
     });
 });
