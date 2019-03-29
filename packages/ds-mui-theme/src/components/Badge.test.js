cb9a81d71c9a97b13a3a151f90e51541e185d1d5
Wladimir Delenclos <wdelenclos@gmail.com>
2019-03-29 16:16:50 +0100

    Revert "BACKLOG-9976 : Fix babel config"
    This reverts commit a7589673f9fed192d758449e422066173235107d.

---
+++ packages/ds-mui-theme/src/components/Badge.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {Badge} from './Badge';
+
+describe('Badge', () => {
+    it('should render correctly in "debug" mode', () => {
+        const component = shallow(<Badge debug />);
+        expect(component).toMatchSnapshot();
+    });
+});
