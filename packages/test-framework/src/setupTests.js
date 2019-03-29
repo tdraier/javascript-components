a620b6234b6b28a8252121c36bdbaa9c9ca3eeb5
Thomas Draier <tdraier@jahia.com>
2019-03-29 17:45:02 +0100

    BACKLOG-9972: Auto mock

--- packages/test-framework/src/setupTests.js
+++ packages/test-framework/src/setupTests.js
@@ -2,3 +2,5 @@ import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
+
+jest.mock('react-i18next', () => require('./__mocks__/react-i18next'));
