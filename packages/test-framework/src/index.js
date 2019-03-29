01dcc3255db86021ff94ff8ac43ececda4de8174
akarmanov <alexandrk_@hotmail.com>
2019-03-29 14:30:52 -0400

    BACKLOG-9982 Add eslint config

--- packages/test-framework/src/index.js
+++ packages/test-framework/src/index.js
@@ -1,3 +1,4 @@
 export {default as jestConfig} from './jest.config';
+export {default as eslintConfig} from './eslint.config';
 export {default as shallowWithTheme} from './utils/shallowWithTheme';
 export * from 'enzyme';
