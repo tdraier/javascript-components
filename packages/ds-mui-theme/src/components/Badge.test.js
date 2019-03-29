11b5b12137f2fd71ccd8cc71d9a0d71ec4fc37b9
Wladimir Delenclos <wdelenclos@gmail.com>
2019-03-29 18:15:13 +0100

    Backlog-9976 : Add test component

--- packages/ds-mui-theme/src/components/Badge.test.js
+++ packages/ds-mui-theme/src/components/Badge.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Badge from './Badge';
 import {shallowWithTheme} from '@jahia/test-framework';
+import {dsGenericTheme} from '../theme';
 
 describe('Badge', () => {
     it('should render a <div />', () => {
-        const wrapper = shallowWithTheme(<Badge/>);
+        const wrapper = shallowWithTheme(<Badge/>, {}, dsGenericTheme);
         expect(wrapper.find('div').length).toEqual(1);
     });
 });
