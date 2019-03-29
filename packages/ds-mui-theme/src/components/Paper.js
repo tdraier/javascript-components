1e60acd1e9c1334977c7e8f61476693dfe11d95d
Wladimir Delenclos <wdelenclos@gmail.com>
2019-03-29 14:32:21 +0100

    DESIGN-143 : Add Paper component

---
+++ packages/ds-mui-theme/src/components/Paper.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Paper as MuiPaper, withStyles} from '@material-ui/core';
+import * as _ from 'lodash';
+import classnames from 'classnames';
+
+let styles = theme => ({
+    root: {
+        margin: '24px 32px',
+        padding: theme.spacing.unit * 4
+    },
+    colorLight: {
+        backgroundColor: theme.palette.ui.epsilon
+    },
+    colorDark: {
+        backgroundColor: theme.palette.ui.gamma,
+        color: theme.palette.invert.beta
+    }
+});
+
+const getClasses = ({color, classes: {root, ...myClasses}}) => ({
+    root: classnames(
+        root,
+        myClasses['color' + _.capitalize(color)],
+    )
+});
+
+const Paper = withStyles(styles, {name: 'DsPaper'})(
+    ({color, classes, ...props}) => (
+        <MuiPaper classes={getClasses({color, classes})} {...props}/>
+    )
+);
+
+Paper.propTypes = process.env.NODE_ENV !== 'production' ? {
+} : {};
+
+Paper.defaultProps = {
+    color: 'light'
+};
+
+Paper.displayName = 'DsPaper';
+
+export default Paper;
