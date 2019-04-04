0ec67af3cde525379773c35385b936d2a2cda023
Thomas Draier <tdraier@jahia.com>
2019-04-04 11:38:37 +0200

    BACKLOG-9916 : added cache flush methods in cache object

--- packages/apollo-dx/src/client.js
+++ packages/apollo-dx/src/client.js
@@ -2,9 +2,9 @@ import {ApolloClient} from 'apollo-client';
 import {from, split} from 'apollo-link';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {getMainDefinition, toIdValue} from 'apollo-utilities';
-import {fragmentMatcher} from "./fragmentMatcher";
+import {fragmentMatcher} from './fragmentMatcher';
 
-import {dxHttpLink, dxSseLink, dxUploadLink, ssrLink} from "./links";
+import {dxHttpLink, dxSseLink, dxUploadLink, ssrLink} from './links';
 
 const client = function (options) {
     options = options || {};
@@ -14,14 +14,14 @@ const client = function (options) {
     // Map of path/uuid to be able to resolve cache key when we only have the path during cache resolving
     let idByPath = {};
 
-    // get final cacke Key
+    // Get final cacke Key
     let getId = (workspace, uuid) => toIdValue(normalizeId(workspace, uuid));
 
-    // get formated cache key
-    let normalizeId = (workspace, uuid) => workspace + ":" + uuid;
+    // Get formated cache key
+    let normalizeId = (workspace, uuid) => workspace + ':' + uuid;
 
-    let dataIdFromObject = (data) => {
-        // use dataIdFromObject() from the options if provided
+    let dataIdFromObject = data => {
+        // Use dataIdFromObject() from the options if provided
         if (options.dataIdFromObject) {
             let r = options.dataIdFromObject(data);
             if (r) {
@@ -29,19 +29,21 @@ const client = function (options) {
             }
         }
 
-        // in order to cache JCR node we need at least uuid and workspace fields
+        // In order to cache JCR node we need at least uuid and workspace fields
         if (data.uuid && data.workspace) {
             if (data.path) {
-                // store key for path, in case queryNodeByPath is used in the future we can resolve the appropriate ID
+                // Store key for path, in case queryNodeByPath is used in the future we can resolve the appropriate ID
                 idByPath[data.path] = data.uuid;
             }
+
             return normalizeId(data.workspace, data.uuid);
         }
 
-        // use default apollo cache key
+        // Use default apollo cache key
         if (data.__typename && data.id) {
-            return normalizeId(data.__typename, data.id)
+            return normalizeId(data.__typename, data.id);
         }
+
         return undefined;
     };
 
@@ -49,39 +51,39 @@ const client = function (options) {
         JCRQuery: {
             nodeById: (_, args) => {
                 if (_.workspace) {
-                    return getId(_.workspace, args.uuid)
+                    return getId(_.workspace, args.uuid);
                 }
             },
             nodesById: (_, args) => {
                 if (_.workspace) {
                     return args.uuids.map(function (uuid) {
                         return getId(_.workspace, uuid);
-                    })
+                    });
                 }
             },
             nodeByPath: (_, args) => {
                 if (_.workspace && idByPath[args.path]) {
-                    return getId(_.workspace, idByPath[args.path])
+                    return getId(_.workspace, idByPath[args.path]);
                 }
             },
             nodesByPath: (_, args) => {
                 if (_.workspace) {
-                    let f = args.paths.map((path) => (idByPath[path] ? getId(_.workspace, idByPath[path]) : undefined));
-                    return f.indexOf(undefined) !== -1 ? undefined : f;
+                    let f = args.paths.map(path => (idByPath[path] ? getId(_.workspace, idByPath[path]) : undefined));
+                    return f.indexOf(undefined) === -1 ? f : undefined;
                 }
             }
         }
     };
 
-    // add JCRNode cache resolvers:
-    for (let typeName of fragmentMatcher.possibleTypesMap["JCRNode"]) {
+    // Add JCRNode cache resolvers:
+    for (let typeName of fragmentMatcher.possibleTypesMap.JCRNode) {
         cacheResolvers[typeName] = {
             nodeInWorkspace: (_, args) => {
                 if (_.uuid) {
-                    return getId(args.workspace, _.uuid)
+                    return getId(args.workspace, _.uuid);
                 }
             }
-        }
+        };
     }
 
     if (options.cacheResolvers) {
@@ -94,21 +96,43 @@ const client = function (options) {
         cacheResolvers: cacheResolvers
     });
 
-    let sseLink = dxSseLink((options.contextPath ? options.contextPath : '')+'/modules/graphql');
+    cache.flushNodeEntryByPath = (path, workspace = 'EDIT') => {
+        let cacheKey = cacheResolvers.JCRQuery.nodeByPath({workspace}, {path});
+        if (cacheKey) {
+            let strings = Object.keys(cache.data.data).filter(key => key.match(new RegExp('.*' + cacheKey.id + '.*')));
+            strings.forEach(key => cache.data.delete(key));
+            return strings.length;
+        }
+
+        return 0;
+    };
+
+    cache.flushNodeEntryById = (uuid, workspace = 'EDIT') => {
+        let cacheKey = cacheResolvers.JCRQuery.nodeById({workspace}, {uuid});
+        if (cacheKey) {
+            let strings = Object.keys(cache.data.data).filter(key => key.match(new RegExp('.*' + cacheKey.id + '.*')));
+            strings.forEach(key => cache.data.delete(key));
+            return strings.length;
+        }
+
+        return 0;
+    };
+
+    let sseLink = dxSseLink((options.contextPath ? options.contextPath : '') + '/modules/graphql');
 
     let httpLink = from([dxUploadLink, dxHttpLink(options.contextPath ? options.contextPath : '', options.useBatch, options.httpOptions)]);
 
     let link = split(
-        // split based on operation type
-        ({ query }) => {
-            const { kind, operation } = getMainDefinition(query);
+        // Split based on operation type
+        ({query}) => {
+            const {kind, operation} = getMainDefinition(query);
             return kind === 'OperationDefinition' && operation === 'subscription';
         },
         sseLink,
         httpLink,
     );
     return new ApolloClient({
-        link: !ssrMode ? (options.link ? options.link : link) : ssrLink,
+        link: ssrMode ? ssrLink : (options.link ? options.link : link),
         cache: cache,
         ssrMode: ssrMode
     });
