b9d477d595b1836543c2344f2ef2a04f3d9cd738
tdraier <tdraier@jahia.com>
2019-05-09 16:53:31 +0200

    BACKLOG-10163 : Fixed test module

--- packages/test-framework/src/index.js
+++ packages/test-framework/src/index.js
@@ -1,4 +1,3 @@
 export {default as jestConfig} from './jest.config';
-export {default as eslintConfig} from './eslint.config';
 export {default as shallowWithTheme} from './utils/shallowWithTheme';
 export * from 'enzyme';
