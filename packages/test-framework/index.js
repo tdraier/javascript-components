2d760c609a528d579f8705a70e1a0dd78e60301a
David Griffon <dgriffon@jahia.com>
2019-05-15 11:44:00 +0200

    BACKLOG-10080 : fix eslint issues

--- packages/test-framework/index.js
+++ packages/test-framework/index.js
@@ -1,16 +1,19 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
     value: true
 });
 
-var _build = require("./build/js");
+var _build = require('./build/js');
 
 Object.keys(_build).forEach(function (key) {
-    if (key === "default" || key === "__esModule") return;
+    if (key === 'default' || key === '__esModule') {
+        return;
+    }
+
     Object.defineProperty(exports, key, {
         enumerable: true,
-        get: function get() {
+        get() {
             return _build[key];
         }
     });
