b57b3bc7316436b9b51cd8dcff0cf35cc86b2222
David Griffon <dgriffon@jahia.com>
2019-05-15 13:55:33 +0200

    BACKLOG-10080 : fix eslint issues

--- packages/react-material/src/layout/ProgressOverlay.jsx
+++ packages/react-material/src/layout/ProgressOverlay.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {PropTypes} from 'prop-types';
+import PropTypes from 'prop-types';
 import {CircularProgress, withStyles} from '@material-ui/core';
 
 const styles = () => ({
